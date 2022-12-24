26bbefe000b936d48961751e2c7bfe26dee112d8
Jimin Kim <0305jimin@gmail.com>
2022-12-25 02:32:16 +0900

    Feat: adding cascade option

--- jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -2,12 +2,14 @@ package jpabook.jpashop.domain;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.bytecode.enhance.spi.interceptor.AbstractLazyLoadInterceptor;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.CascadeType.ALL;
 import static javax.persistence.FetchType.LAZY;
 
 @Entity
@@ -25,11 +27,12 @@ public class Order {
     @JoinColumn(name = "member_id") // FK 이름이 member_id
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    // cascade = CascadeType.ALL
+    @OneToMany(mappedBy = "order", cascade = ALL) // persist 전파, delete할 때 같이 지움
     private List<OrderItem> orderItems = new ArrayList<>();
 
 
-    @OneToOne(fetch = LAZY)
+    @OneToOne(fetch = LAZY, cascade = ALL)
     @JoinColumn(name = "delivery_id") // 연관관계의 주인
     // 일대일 관계에서는 FK를 어느 쪽에든 둬도 되는데
     // 접근을 더 많이 하는 곳에 두는 게 편하다.
@@ -39,7 +42,8 @@ public class Order {
 
 //    private Date date; 로 하면 annotation 매핑을 해야 하지만
     private LocalDateTime orderDate; // Java 8에서는 이를 쓰면 된다. 시, 분까지 있다
-
+    // 스프링 부트; SpringPhysicalNamingStrategy 에 따라서 order_date로 바꾼다.
+    // Java의 Camel case -> under score & lower case로 바꿔버린다.
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // 주문의 상태 [ORDER, CANCEL]
 
