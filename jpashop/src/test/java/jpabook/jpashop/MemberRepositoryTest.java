4fa3c1c1e54b81e663ba6cf6639616ca32d72b89
Jimin Kim <0305jimin@gmail.com>
2022-12-24 17:47:21 +0900

    Chore: set rollback setting, verifying characteristic of entity #1

--- jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,6 +21,10 @@ public class MemberRepositoryTest {
     // tdd 쓰고 탭하면 만들어지게 할 수 있다. Live template에 설정하면  intelliJ에서 해준다.
     @Test
     @Transactional // 스프링 것을 쓰기를 권장 - 쓸 수 있는 옵션이 많기 때문
+    // Transactional 어노테이션이 테스트케이스에 있으면 테스트가 끝난 다음에 바로 롤백을 해버린다.
+    // 테스트가 아닌 곳에 있으면 정상적으로 동작한다. 테스트에 있으면 디비를 롤백해 버린다.
+    // 데이터가 들어가 있으면 반복적인 테스트를 못하니까 이게 맞긴 하다.
+    @Rollback(false)
     public void testMember() throws Exception{
         // given
         Member member = new Member();
@@ -32,7 +37,9 @@ public class MemberRepositoryTest {
         // then
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
         Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
-
+        Assertions.assertThat(findMember).isEqualTo(member);
+        System.out.println("findMember == member:"+ (findMember == member));
+        // 같은 영속성 컨텍스트 안에서 id 값이 같으면 같은 엔티티
     }
 
 }
