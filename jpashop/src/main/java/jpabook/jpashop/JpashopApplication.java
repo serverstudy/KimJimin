0f4a0eb41baf006ca2825e91fb802fab1a1077ea
Jimin Kim <0305jimin@gmail.com>
2022-12-24 15:49:34 +0900

    Chore: downloading h2 #1

--- jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
+++ jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -8,12 +8,6 @@ public class JpashopApplication {
 
 
 	public static void main(String[] args) {
-
-		Hello hello = new Hello();
-		hello.setData("hello");
-String data = hello.getData();
-// sout 쓰면 자동 완성된다.
-		System.out.println("data = "+ data);
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
