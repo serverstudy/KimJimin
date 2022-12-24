2530c1edbe749d991107ad72daa8e8d93f6f580f
Jimin Kim <0305jimin@gmail.com>
2022-12-25 02:49:16 +0900

    Docs: annotating about field initializing #1

--- jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -22,5 +22,12 @@ public class Member {
     private Address address;
 
     @OneToMany(mappedBy = "member") // '연관관계의 주인이 아닌 거울이다'
-    private List<Order> orders = new ArrayList<>();
+    private List<Order> orders = new ArrayList<>(); // 초기화를 생성자에서 해줄 수도 있지만 이게 best practice
+    // null 문제에서 안전
+    // hibernate가 entity를 persist 하는 순간(em.persist(~~)) collection을 감싸버려서
+    // hibernate가 제공하는 내장 collection으로 변경돼 버린다.
+    // -> hibernate가 변경 사항을 추적해야 하기 때문에 그렇다.
+    // 근데 set해서 또 바꿔버리면 변경 사항 추적이 안된다.
+    // -> 가급적이면 수정하지도, 밖으로 꺼내지도 않는 것이 좋다. 컬렉션 자체를 바꾸지를 말기.
+    // 이런 이유에서도 collection은 field에서 초기화하는 것이 가장 좋다.
 }
