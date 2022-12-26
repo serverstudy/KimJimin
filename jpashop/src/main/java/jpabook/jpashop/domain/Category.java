8f40064101f046b9b33b58c7b681ebef8b64915f
Jimin Kim <0305jimin@gmail.com>
2022-12-26 18:43:58 +0900

    Feat: membership joining test #3

--- jpashop/src/main/java/jpabook/jpashop/domain/Category.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Category.java
@@ -35,7 +35,7 @@ public class Category {
     private List<Category> child = new ArrayList<>();
 
     //==연관관계 편의 메서드==//
-    public void addChildCategory(Category category){
+    public void addChildCategory(Category child){
         this.child.add(child);
         child.setParent(this);
     }
---
+++ jpashop/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,49 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class) // 스프링이랑 integration해서 테스트할 거라서
+@SpringBootTest // 스프링이랑 integration해서 테스트할 거라서
+@Transactional // 데이터 변경해야 하니까
+// persist로 insert 쿼리가 가는 것은 커밋을 할 때인데 스프링에서 Transactional은 커밋이 아니라 롤백을 한다.
+public class MemberServiceTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+    @Autowired EntityManager em;
+    @Test
+//    @Rollback(false) // 롤백 안하고 커밋. 또는 em 만들어 flush
+    public void 회원가입() throws Exception {
+        // given
+        Member member = new Member();
+        member.setName("kim");
+
+        // when
+        Long savedId = memberService.join(member);
+
+        // then
+        em.flush(); // 영속성 컨텍스트에 있는 객체가 쿼리로 디비에 반영된다.
+        assertEquals(member, memberRepository.findOne(savedId));
+    }
+
+    @Test
+    public void 중복_회원_예외() throws Exception {
+        // given
+
+        // when
+
+        // then
+    }
+}
