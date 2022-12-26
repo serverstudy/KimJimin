a4f3446189f1ed4df967ffd2c8ab67dab085d557
Jimin Kim <0305jimin@gmail.com>
2022-12-26 17:58:48 +0900

    Refactor: using @RequiredArgsConstructor #3

--- jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,22 +1,30 @@
 package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository // component 스케일에서 자동으로 스프링 빈으로 등록, 관리가 된다.
+@RequiredArgsConstructor
 public class MemberRepository {
 
-    @PersistenceContext // JPA가 제공하는 표준 어노테이션
-    private EntityManager em; // Entity Manager를 만들어서 주입해 준다.
+//    @PersistenceContext // JPA가 제공하는 표준 어노테이션
+//    @Autowired // EntityManager는 @PersistenceContext만 되는데, 스프링 부트 JPA를 쓰면 이걸로 바꾸기 가능
+    private final EntityManager em; // Entity Manager를 만들어서 주입해 준다.
 
     // 만약 엔티티 매니저 팩토리를 주입하고 싶다면 아래 코드.
     // 원래 순수 JPA를 쓰면 EntityManagerFactory에서 직접 EntityManager를 꺼내 써야 한다.
 //    @PersistenceUnit
 //    private EntityManagerFactory emf;
+
+    // @RequiredArgsConstructor 써줬으니까 자동으로 생성된다.
+    // public MemberRepository(EntityManager em){
+    //     this.em = em;
+    // }
+
     public void save(Member member){
         em.persist(member); // JPA가 저장
         // 영속성 컨텍스트 안에 멤버 객체를 넣고
