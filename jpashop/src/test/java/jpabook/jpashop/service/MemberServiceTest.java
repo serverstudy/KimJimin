3b3a74e75d28e2ec15a7a7b65d4ebf46533cbcef
Jimin Kim <0305jimin@gmail.com>
2022-12-26 18:52:45 +0900

    Feat: validating duplicate member test #3

--- jpashop/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ jpashop/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,7 +16,7 @@ import static org.junit.Assert.*;
 @RunWith(SpringRunner.class) // 스프링이랑 integration해서 테스트할 거라서
 @SpringBootTest // 스프링이랑 integration해서 테스트할 거라서
 @Transactional // 데이터 변경해야 하니까
-// persist로 insert 쿼리가 가는 것은 커밋을 할 때인데 스프링에서 Transactional은 커밋이 아니라 롤백을 한다.
+// persist로 insert 쿼리가 가는 것은 커밋을 할 때인데 테스트 코드에서의 Transactional은 커밋이 아니라 롤백을 한다.
 public class MemberServiceTest {
 
     @Autowired MemberService memberService;
@@ -38,12 +37,26 @@ public class MemberServiceTest {
         assertEquals(member, memberRepository.findOne(savedId));
     }
 
-    @Test
+    @Test(expected = IllegalStateException.class)
     public void 중복_회원_예외() throws Exception {
         // given
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
 
         // when
+//        memberService.join(member1);
+//        try {
+//            memberService.join(member2); // 예외가 발생해야 한다.
+//        } catch (IllegalStateException e){
+//            return;
+//        }
+        memberService.join(member1);
+        memberService.join(member2);
 
         // then
+        fail("예외가 발생해야 한다.");
     }
 }
