06b1e4e37c1ffad6a2237f1610ea618ccfd1a8d1
Jimin Kim <0305jimin@gmail.com>
2022-12-26 19:37:13 +0900

    Feat: Item Service #3

---
+++ jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService { // ItemRepository에 단순히 위임만 하는 클래스
+    private final ItemRepository itemRepository;
+
+    @Transactional // 메소드에 가까운 것이 우선권을 가진다.
+    public void saveItem(Item item){
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems(){
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId){
+        return itemRepository.findOne(itemId);
+    }
+}
