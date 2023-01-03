5c333d24549e2c238292a9850df96c6fc6ff9ba1
Jimin Kim <0305jimin@gmail.com>
2023-01-03 23:04:12 +0900

    Feat: create order #5

---
+++ jpashop/src/main/java/jpabook/jpashop/controller/OrderController.java
@@ -0,0 +1,47 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.service.ItemService;
+import jpabook.jpashop.service.MemberService;
+import jpabook.jpashop.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+    private final MemberService memberService;
+    private final ItemService itemService;
+
+    @GetMapping("/order")
+    public String createForm(Model model){
+
+        List<Member> members = memberService.findMembers();
+        List<Item> items = itemService.findItems();
+
+        model.addAttribute("members", members);
+        model.addAttribute("items", items);
+
+        return "order/orderForm";
+    }
+
+    @PostMapping("/order")
+    public String order(@RequestParam("memberId") Long memberId,
+                        @RequestParam("itemId") Long itemId,
+                        @RequestParam("count") int count) {
+
+        orderService.order(memberId, itemId, count);
+        // 컨트롤러에서는 식별자만 넘기고 비즈니스 로직은 엔티티/서비스
+        // -> 영속성 컨텍스트 안에서 처리 가능
+        return "redirect:/orders";
+    }
+}
