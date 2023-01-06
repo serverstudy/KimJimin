1dfbeb73900868ec644d64af006cb8d44e8b5cec
Jimin Kim <0305jimin@gmail.com>
2023-01-06 15:54:59 +0900

    Feat: update user info #7

--- jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -2,13 +2,11 @@ package jpabook.jpashop.api;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
@@ -33,12 +31,22 @@ public class MemberApiController {
     public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request){
         // API를 개발할 땐 엔티티를 파라미터로 받지 말기.
         // 엔티티 함부로 노출시키지 말기.
-        
+
         Member member = new Member();
         member.setName(request.getName());
 
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
+    }
+
+    // PUT은 멱등하다. 같은 걸 여러 번 호출해도 결과가 똑같다.
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(@PathVariable("id") Long id,
+                                               @RequestBody @Valid UpdateMemberRequest request){
+        // 커맨드와 쿼리 분리 -  유지보수성 증대
+        memberService.update(id, request.getName()); // 커맨드
+        Member findMember = memberService.findOne(id); // 쿼리
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
     }
     @Data
     static class CreateMemberResponse {
@@ -52,6 +60,18 @@ public class MemberApiController {
     @Data
     static class CreateMemberRequest {
         @NotEmpty
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse{
+        private Long id;
+        private String name;
+    }
+
+    @Data
+    static class UpdateMemberRequest{
         private String name;
     }
 }
