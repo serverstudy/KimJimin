5290c3b347020cdd4bfdb0d57711810bf33521b9
Jimin Kim <0305jimin@gmail.com>
2023-01-11 14:26:30 +0900

    Feat: v1 getting list of orders ;errorsome example #9

---
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,33 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jpabook.jpashop.domain.Order;
+import java.util.List;
+
+/**
+ * xToOne
+ *
+ * Order
+ * Order -> Member (ManyToOne)
+ * Order -> Delivery (OneToOne)
+ */
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1(){
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        // Order에 Member가 있어 Member로 가면 Member에 Order가 있어 또 Order로 가는 무한루프에 빠진다.
+        // 양방향 연관관계 문제가 생긴다. 
+        return all;
+    }
+}
