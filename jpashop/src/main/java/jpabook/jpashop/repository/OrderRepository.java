3df2f937aaadd801d88f97229b465a067f121614
Jimin Kim <0305jimin@gmail.com>
2023-09-11 12:39:41 +0900

    Feat: adding where statement with queryDsl #14

--- jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop.repository;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.domain.QMember;
 import jpabook.jpashop.domain.QOrder;
 import jpabook.jpashop.repository.order.simplequery.SimpleOrderQueryDto;
@@ -105,16 +107,25 @@ public class OrderRepository {
     }
 
     public List<Order> findAll(OrderSearch orderSearch) {
+        JPAQueryFactory query = new JPAQueryFactory(em);
         QOrder order = QOrder.order;
         QMember member = QMember.member;
 
-        JPAQueryFactory query = new JPAQueryFactory(em);
+
         return query
                 .select(order)
                 .from(order)
                 .join(order.member, member)
+                .where(statusEq(orderSearch.getOrderStatus()), member.name.like(orderSearch.getMemberName()))
                 .limit(1000)
                 .fetch();
+    }
+
+    private BooleanExpression statusEq(OrderStatus statusCond) {
+        if (statusCond == null) {
+            return null;
+        }
+        return QOrder.order.status.eq(statusCond);
     }
 
     public List<Order> findAllWithMemberDelivery() {
