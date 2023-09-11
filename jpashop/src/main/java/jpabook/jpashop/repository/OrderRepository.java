af6074774c0135b64579a4838e24148a18c777df
Jimin Kim <0305jimin@gmail.com>
2023-09-11 12:58:27 +0900

    Refactor: reducing codes by making constructor #14

--- jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -21,10 +21,15 @@ import static jpabook.jpashop.domain.QMember.member;
 import static jpabook.jpashop.domain.QOrder.order;
 
 @Repository // JPA를 직접 사용하는 계층, 엔티티 매니저 사용
-@RequiredArgsConstructor
 public class OrderRepository {
 
     private final EntityManager em;
+    private final JPAQueryFactory query;
+
+    public OrderRepository(EntityManager em) {
+        this.em = em;
+        this.query = new JPAQueryFactory(em);
+    }
 
     public void save(Order order){
         em.persist(order);
@@ -110,8 +115,6 @@ public class OrderRepository {
     }
 
     public List<Order> findAll(OrderSearch orderSearch) {
-        JPAQueryFactory query = new JPAQueryFactory(em);
-
         return query
                 .select(order)
                 .from(order)
